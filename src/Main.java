47bfb9cc85b89d0bbc2edbf6180b1df849ea3c4b
charm-11 <cyomorong@gmail.com>
2025-07-20 15:26:00 -0700

    change numbers

--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,7 @@ public class Main {
      public static void main(String[] args) {
          System.out.println("Cyomoro Ngabo Paulin");
          int a = 13;
-         int b = 123;
+         int b = 35;
          System.out.println(a+b);
     }
 }
