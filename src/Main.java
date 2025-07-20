8465cc226e91676e4b3b819240aa44051f5cf100
charm-11 <cyomorong@gmail.com>
2025-07-20 15:23:04 -0700

    added two nbrs

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,9 @@
 public class Main {
+
      public static void main(String[] args) {
          System.out.println("Cyomoro Ngabo Paulin");
+         int a = 13;
+         int b = 123;
+         System.out.println(a+b);
     }
 }
